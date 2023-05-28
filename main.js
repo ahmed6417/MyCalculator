1b34b60f16a4a941235d135a40a056c790be68f2
AhmedHossam <ahmedhossam6417@gmail.com>
2023-05-28 13:48:08 +0300

    Publish My App

---
+++ main.js
@@ -0,0 +1,29 @@
+function sum(){
+    let a =Number(document.getElementById("firstNumber").value);
+    let b =Number(document.getElementById("secondNumber").value);
+    
+    alert(a + b);
+    }
+
+
+
+
+
+    function suminbox(){
+
+        document.getElementById('result').style.display = "block";
+
+        // document.getElementById('hideif').style.display = "none";
+
+        let c =document.getElementById("firstNumber").value;
+        
+        let d =document.getElementById("secondNumber").value;
+        
+        document.getElementById("result").value = Number(c) + Number(d);
+        }
+    
+        // resultDisplay.text= document.getElementById("result").value;
+        
+        
+        
+    
